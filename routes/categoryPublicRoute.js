c1cb736407b4e79e55ffa44ce7e8c9d3dbcae2b9
addityaasharma <adityasharma96655@gmail.com>
2025-05-06 16:56:29 +0530

    changes done

--- routes/categoryPublicRoute.js
+++ routes/categoryPublicRoute.js
@@ -3,7 +3,7 @@ import Category from '../models/CategoryModel.js';
 
 const router = express.Router();
 
-export default router.get('/',async(req,res)=>{
+router.get('/',async(req,res)=>{
     try{
         const categories = await Category.find();
         if(!categories) return res.status(404).json({message : "No categories found"});
@@ -12,3 +12,24 @@ export default router.get('/',async(req,res)=>{
         res.status(404).json({message : 'Error fetching Categories', error : err})
     }
 });
+
+router.get("/:link", async (req, res) => {
+  try {
+    const { link } = req.params;
+
+    const article = await News.findOne({ link })
+      .populate("category", "name")
+      .populate("author", "username email");
+
+    if (!article) {
+      return res.status(404).json({ message: "Article not found." });
+    }
+
+    res.status(200).json({ article });
+  } catch (err) {
+    console.error("Error fetching article by link:", err);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
+
+export default router;
